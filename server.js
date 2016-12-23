4f234f96f72f4ad757ed8e11ea4df7331c472219
ingpatrickls <ingpatrickls@gmail.com>
2016-12-23 13:23:59 -0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+// middleware
+app.use((req, res, next) => {
+    var now = new Date().toString()
+    var log = (`${now}:${req.method} ${req.url}`);
+
+    console.log(log);
+
+    fs.appendFile('server.log', log + '\n');
+    next();
+});
+
+//maintenance
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+});
+
+//pour eviter que la page de maintenane ne s'affiche la premiere
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+//hbs.registerHelper('screamIt',(text)=>{
+// return text.toUpperCase();
+//});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'welcome to my Website'
+
+    });
+});
+
+app.get('/about', (req, res) => {
+    // res.send('about page');
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+
+    });
+
+});
+
+// send back json with error message
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'unable to handle request'
+    });
+});
+app.listen(3000, () => { console.log('Server is up at port 3000'); }); 
